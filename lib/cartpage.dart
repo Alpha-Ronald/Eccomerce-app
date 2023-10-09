9a53c827082d5bf8f191c243b7dfd05dd09c6f3b
Alpha-Ronald <ronnieadewoye@gmail.com>
2023-10-09 23:06:05 +0100

    (Cart dev)- cart page dev

--- lib/cartpage.dart
+++ lib/cartpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_projects/blocstate.dart';
 
 class CartList extends StatefulWidget {
   const CartList({super.key});
@@ -9,6 +10,15 @@ class CartList extends StatefulWidget {
 
 class CartListState extends State<CartList> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      cartItems = BlocProvider.of<CartBloc>(context).items;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +34,61 @@ class CartListState extends State<CartList> {
       ),
       body: GestureDetector(
         onTap: () => FocusScope.of(context).unfocus(),
-        child: const Column(
+        child: Column(
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            //cannot proceed until i build the statemanagemt
-            //BlocBuilder<CartBloc, CartState>(builder: (cartState){
+            Expanded(
+                child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  BlocBuilder<CartBloc, CartState>(builder: (cartState) {
+                    return ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      itemCount: cartItems.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(16, 8, 16, 0),
+                          child: Container(
+                            width: double.infinity,
+                            height: 100,
+                            decoration: BoxDecoration(
+                                color: Colors.white10,
+                                borderRadius: BorderRadius.circular(12)),
+                            child: Padding(
+                              padding:
+                                  EdgeInsetsDirectional.fromSTEB(16, 8, 8, 8),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Hero(
+                                      tag: 'ControllerImage',
+                                      transitionOnUserGestures: true,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(12),
+                                        child: Image.network(
+                                          cartItems[index].image,
+                                          width: 80,
+                                          height: 80,
+                                          fit: BoxFit.fitWidth,
+                                        ),
+                                      ))
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  })
+                ],
+              ),
+            ))
           ],
         ),
       ),
